6c7b1ec0c1dd62fe11b0c033396f9ed87ba06697
dannyljl <dannylee1997@hotmail.com>
2020-04-23 16:23:20 +0200

    started changing code from one tutorial to the other

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GuildComponent} from './guild/guild.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'guild',
+    pathMatch: 'full'
+  },
+  {
+    path: 'guild',
+    component: GuildComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/guild/guild.component.ts
+++ src/app/guild/guild.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {WebSocketAPI} from './WebSocketAPI';
 
 @Component({
   selector: 'app-guild',
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guild.component.css']
 })
 export class GuildComponent implements OnInit {
+  title = 'angular9-springboot-websocket';
+  input;
+  webSocketAPI: WebSocketAPI;
+  greeting = [];
+  name: string;
 
-  constructor() { }
+  get getGreeting(){
+    return this.greeting;
+  }
 
-  ngOnInit(): void {
+  set setGreeting(greeting){
+    this.greeting = greeting;
+  }
+  ngOnInit() {
+    this.webSocketAPI = new WebSocketAPI(new GuildComponent());
+  }
+
+  connect(){
+    this.webSocketAPI._connect();
+  }
+
+  disconnect(){
+    this.webSocketAPI._disconnect();
+  }
+
+  sendMessage(){
+    this.webSocketAPI._send(this.name);
+  }
+
+  handleMessage(message) {
+    this.title = message;
+    this.greeting.push(message);
+    for(let msg in this.greeting){
+      console.log(msg.toString());
+    }
   }
 
 }
